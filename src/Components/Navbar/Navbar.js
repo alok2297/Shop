97a8ef5b2ddde0d514bf9f1305d7bb879535e39f
alok2297 <72278166+alok2297@users.noreply.github.com>
2024-02-24 00:41:50 +0530

    Api intergration login

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,20 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import './Navbar.css';
 import logo from '../Asset/logo.png';
 import cart_icon from '../Asset/cart_icon.png';
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../../Context/ShopContext';
+import nav_dropdown from '../Asset/dropdown.png';
 const Navbar = () => {
     // state for the navbar
     const [menu,setmenu] = useState("shop");  
     const {getTotalCartItems} = useContext(ShopContext);
+    const menuRef = useRef();
+
+    const dropdown_toggle=(e)=>{
+      menuRef.current.classList.toggle('nav-menu-visible');
+      e.target.classList.toggle('open');
+    }
 
   return (
     <div className='navbar'>
@@ -15,7 +22,8 @@ const Navbar = () => {
         <img src={logo} alt='$'/>
         <p>SHOPIFY</p>
       </div>
-      <ul className='nav-menu'>
+      <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt=''/>
+      <ul ref={menuRef} className='nav-menu'>
          <li onClick={()=>{setmenu("shop")}}><Link to='/' style={{textDecoration:'none'}}>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
          <li onClick={()=>{setmenu("mens")}}><Link to='/mens' style={{textDecoration:'none'}}>MenShop</Link>{menu==="mens"?<hr/>:<></>}</li>
          <li onClick={()=>{setmenu("womens")}}><Link to='/womens' style={{textDecoration:'none'}}>WomenShop</Link>{menu==="womens"?<hr/>:<></>}</li>
--- src/Pages/LoginSignup.js
+++ src/Pages/LoginSignup.js
@@ -1,17 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/LoginSignUp.css';
 const LoginSignup = () => {
+  const [state,setState] = useState("Login");
+  const [formData,setformData] = useState({
+    username:"",
+    email:"",
+    password:""
+  })
+  const changeHandler =(e)=>{
+    setformData({...formData,[e.target.name]:e.target.value})
+  }
+  console.log(formData);
+  const handlebtn=()=>{
+    state==="Login"?setState("SignUp"):setState("Login")
+  }
+
+  const login=()=>{
+
+  }
+  const signup= async()=>{
+    const url = 'http://localhost:5000/signup'
+    const requestOptions={
+      method:'POST',
+      headers:{
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(formData)
+    }
+    await fetch(url,requestOptions)
+    .then((res)=>{
+      if(!res.ok){
+        throw new Error("Network response is not Ok.");
+      }
+      return res.json();
+    }).then((data)=>{
+      if(data.success){
+        localStorage.setItem('auth-token',data.token)
+        window.location.replace("/");
+      }
+      else{
+        alert("Existing User find with the same email Adress");
+      }
+    }).catch(err=>{
+      console.error("Some error while authentication",err);
+    })
+  }
   return (
     <div className='loginSignup'>
       <div className='loginSignup-container'>
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className='loginsignup-fields'>
-          <input type='text' placeholder='Your Name'/>
-          <input type='email' placeholder='Email Adress'/>
-          <input type='password' placeholder='Password'/>
+          {state==="SignUp"?<input defaultValue={formData.username} onChange={changeHandler} name='username' type='text' placeholder='Your Name'/>:<></>}
+          <input name='email' defaultValue={formData.email} onChange={changeHandler} type='email' placeholder='Email Adress'/>
+          <input name='password' defaultValue={formData.password} onChange={changeHandler} type='password' placeholder='Password'/>
         </div>
-        <button>Continue</button>
-        <p className='loginsignup-login'>Alredy have an account?<span style={{marginLeft:"4px"}}>Login</span></p>
+        <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
+        <p className='loginsignup-login'>{state==="SignUp"?"Alredy have an account?":"Create an Account?"}<span onClick={handlebtn} style={{marginLeft:"4px"}}>Login</span></p>
         <div className='loginsignup-agree'>
           <input type='checkbox' name='' id=''/>
           <p>By Continuing, i agree to the terms of use & privacy policy.</p>
